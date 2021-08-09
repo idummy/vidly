8ba60a71dfbd4a075df12770a4a2cf61615970bf
Roshan Sharma <roshan_sharma@amicusglobal.com>
2021-08-09 22:24:47 +0530

    added local project files

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            //return View(movie);
+            return RedirectToAction("Index", "Home",new {id = 123,value = "posh"});
+        }
+
+        public ActionResult Index(int ? id)
+        {
+            if (!id.HasValue)
+            {
+                return Content("No Id Supplied");
+            }
+            else
+            {
+            return Content($"id = {id}");
+            }
+        }
+    }
+}
